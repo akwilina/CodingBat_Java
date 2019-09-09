24b2b16e4f7dca522e82fde60be4c0659082b674
delola\delola lokal <aleksandra.man@gmail.com>
2019-09-09 15:23:12 +0200

    The parameter weekday is true if it is a weekday, and the parameter vacation is true if we are on vacation. We sleep in if it is not a weekday or we're on vacation. Return true if we sleep in.

--- src/codingbat/java/warmup1/SleepIn.java
+++ src/codingbat/java/warmup1/SleepIn.java
@@ -9,7 +9,6 @@ public class SleepIn {
     }
 
     public static boolean sleepIn (boolean weekday, boolean vacation) {
-
         if (!weekday || vacation) {
             return true;
         } else {

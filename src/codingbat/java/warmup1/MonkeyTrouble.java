2a522326cef5a7cb7b98c28d6c0ac7b9f2e09821
delola\delola lokal <aleksandra.man@gmail.com>
2019-09-09 15:28:51 +0200

    We have two monkeys, a and b, and the parameters aSmile and bSmile indicate if each is smiling. We are in trouble if they are both smiling or if neither of them is smiling. Return true if we are in trouble.

---
+++ src/codingbat/java/warmup1/MonkeyTrouble.java
@@ -0,0 +1,17 @@
+package codingbat.java.warmup1;
+
+public class MonkeyTrouble {
+    public static void main(String[] args) {
+        System.out.println(monkeyTrouble(true, true)); //→ true
+        System.out.println(monkeyTrouble(false, false)); //→ true
+        System.out.println(monkeyTrouble(true, false)); // → false;
+    }
+
+    public static boolean monkeyTrouble (boolean aSmile, boolean bSmile) {
+        if (aSmile && bSmile || !aSmile && !bSmile) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}

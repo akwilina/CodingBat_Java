9950bc177eb23e86d9190a66a619435bfb2e71bf
delola\delola lokal <aleksandra.man@gmail.com>
2020-11-23 22:39:39 +0100

    mapAB3 CodingBat solution

---
+++ src/codingbat/java/maps/MapAB3.java
@@ -0,0 +1,25 @@
+package codingbat.java.maps;
+
+/*
+Modify and return the given map as follows: if exactly one of the keys "a" or "b" has a value in the map (but not both), set the other to have that same value in the map.
+
+mapAB3({"a": "aaa", "c": "cake"}) → {"a": "aaa", "b": "aaa", "c": "cake"}
+mapAB3({"b": "bbb", "c": "cake"}) → {"a": "bbb", "b": "bbb", "c": "cake"}
+mapAB3({"a": "aaa", "b": "bbb", "c": "cake"}) → {"a": "aaa", "b": "bbb", "c": "cake"}
+ */
+
+import java.util.Map;
+
+public class MapAB3 {
+    public Map<String, String> mapAB3(Map<String, String> map) {
+
+        if(map.containsKey("a") && !map.containsKey("b")){
+            String value = map.get("a");
+            map.put("b", value);
+        } else if (map.containsKey("b") && !map.containsKey("a")){
+            String value = map.get("b");
+            map.put("a", value);
+        }
+        return map;
+    }
+}

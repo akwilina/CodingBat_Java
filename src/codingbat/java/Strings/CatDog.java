837a444f75db380ae928b854180cc0861a2a00ca
delola\delola lokal <aleksandra.man@gmail.com>
2020-11-24 19:00:49 +0100

    CatDog String-2 CodingBat solution

---
+++ src/codingbat/java/Strings/CatDog.java
@@ -0,0 +1,41 @@
+package codingbat.java.Strings;
+
+public class CatDog {
+/*
+Return true if the string "cat" and "dog" appear the same number of times in the given string.
+
+catDog("catdog") → true
+catDog("catcat") → false
+catDog("1cat1cadodog") → true
+ */
+    public static void main(String[] args) {
+        catDog("catdog");
+        catDog("catcat");
+        catDog("1cat1cadodog");
+        catDog("catxdogxdogxca");
+    }
+
+    public static boolean catDog(String str) {
+        boolean result = false;
+        int catOccur = 0;
+        int dogOccur = 0;
+
+        str = str.replaceAll("[//s]+", "");
+
+        char[] charArray = str.toCharArray();
+
+        for(int i = 0; i < charArray.length-2; i++) {
+            if(charArray[i] == 'c' && charArray[i+1] == 'a' && charArray[i+2] == 't') {
+                catOccur += 1;
+            } else if (charArray[i] == 'd' && charArray[i+1] == 'o' && charArray[i+2] == 'g') {
+                dogOccur += 1;
+            }
+        }
+
+        if(catOccur == dogOccur) {
+            result = true;
+        }
+
+        return result;
+    }
+}

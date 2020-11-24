09be87bbb40ed778c61c8d27ab7587f5c0fe9909
delola\delola lokal <aleksandra.man@gmail.com>
2020-11-24 17:46:22 +0100

    String-2 DoubleChar CodingBat solution

---
+++ src/codingbat/java/maps/Strings/DoubleChar.java
@@ -0,0 +1,28 @@
+package codingbat.java.maps.Strings;
+
+/*
+Given a string, return a string where for every char in the original, there are two chars.
+
+doubleChar("The") → "TThhee"
+doubleChar("AAbb") → "AAAAbbbb"
+doubleChar("Hi-There") → "HHii--TThheerree"
+ */
+
+public class DoubleChar {
+
+    public static void main(String[] args) {
+        doubleChar("The");
+    }
+
+    public static String doubleChar(String str) {
+        String result = "";
+        char[] wordArray = str.toCharArray();
+
+        for(int i = 0; i < wordArray.length; i++){
+            char character = wordArray[i];
+            result = result + character + character;
+        }
+
+        return result;
+    }
+}

7f38a4b5471ba97d7ba681be5a02c8d48e02f0ee
delola\delola lokal <aleksandra.man@gmail.com>
2020-11-24 18:24:06 +0100

    String-2 CountHi CodingBat solution

---
+++ src/codingbat/java/maps/Strings/CountHi.java
@@ -0,0 +1,27 @@
+package codingbat.java.maps.Strings;
+/*
+Return the number of times that the string "hi" appears anywhere in the given string.
+
+countHi("abc hi ho") → 1
+countHi("ABChi hi") → 2
+countHi("hihi") → 2
+ */
+public class CountHi {
+
+    public static void main(String[] args) {
+        countHi("abc hi ho");
+    }
+
+    public static int countHi(String str) {
+        int result = 0;
+        str = str.replaceAll("[\\s]+", "");
+        char[] charArray = str.toCharArray();
+
+        for (int i = 0; i < charArray.length - 1; i++) {
+            if (charArray[i] == 'h' && charArray[i + 1] == 'i') {
+                result += 1;
+            }
+        }
+        return result;
+    }
+}

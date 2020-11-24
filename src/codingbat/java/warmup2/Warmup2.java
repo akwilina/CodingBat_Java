30b7b27061c297da3d495d42f3daefab435108e9
delola\delola lokal <aleksandra.man@gmail.com>
2020-11-24 21:26:13 +0100

    stringTimes Warmup-2 CodingBat solutions

--- src/codingbat/java/warmup2/Warmup2.java
+++ src/codingbat/java/warmup2/Warmup2.java
@@ -1,5 +1,13 @@
 package codingbat.java.warmup2;
 
+/*
+Given a string and a non-negative int n, return a larger string that is n copies of the original string.
+
+stringTimes("Hi", 2) → "HiHi"
+stringTimes("Hi", 3) → "HiHiHi"
+stringTimes("Hi", 1) → "Hi"
+ */
+
 public class Warmup2 {
 
     public static void main(String[] args) {

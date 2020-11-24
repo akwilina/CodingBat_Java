2fc29db0bbcf40a6b16c132f4d3750fc7caa0837
delola\delola lokal <aleksandra.man@gmail.com>
2020-11-24 21:02:33 +0100

    Palindrome (my task)

---
+++ src/codingbat/java/Strings/Palindrome.java
@@ -0,0 +1,27 @@
+package codingbat.java.Strings;
+
+public class Palindrome {
+
+    public static void main(String[] args) {
+        isPalindrome("otto");
+        isPalindrome("toot");
+        isPalindrome("toto");
+        isPalindrome("mamamaaammmmama");
+        isPalindrome("TaaT");
+        isPalindrome("taAt");
+        isPalindrome("ta at");
+    }
+
+    public static boolean isPalindrome (String str) {
+        boolean result = true;
+
+        for(int i = 0; i < str.length()-1; i++) {
+            if (str.charAt(i) != str.charAt(str.length() - 1 - i)) {
+                result = false;
+                break;
+            }
+        }
+        System.out.println(result);
+        return result;
+    }
+}

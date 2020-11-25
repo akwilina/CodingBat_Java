342b0ab095412d4b9f16b55d8447805af769ecb7
delola\delola lokal <aleksandra.man@gmail.com>
2020-11-25 22:50:17 +0100

    makePi Arrays-1 CodingBat solution

---
+++ src/codingbat/java/arrays1/MakePi.java
@@ -0,0 +1,16 @@
+package codingbat.java.arrays1;
+
+/*
+Return an int array length 3 containing the first 3 digits of pi, {3, 1, 4}.
+ */
+
+public class MakePi {
+
+    public static void main(String[] args) {
+        makePi();
+    }
+
+    public static int[] makePi() {
+        return new int[]{3, 1, 4};
+    }
+}

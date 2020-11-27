b652e748b9f48027a77d9e64b498be953d28332d
delola\delola lokal <aleksandra.man@gmail.com>
2020-11-27 20:25:17 +0100

    class Person task_1 person package

---
+++ src/person/Person.java
@@ -0,0 +1,42 @@
+package person;
+
+public class Person {
+
+    private String firstName;
+    private String lastName;
+    private int age;
+    private char sex;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public char getSex() {
+        return sex;
+    }
+
+    public void setSex(char sex) {
+        this.sex = sex;
+    }
+
+}

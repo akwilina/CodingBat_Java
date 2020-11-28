e331ea231cc11c2b2a426188ec39d33437e07fa7
delola\delola lokal <aleksandra.man@gmail.com>
2020-11-28 21:03:22 +0100

    min method basicList package

--- src/basicsLists/Application.java
+++ src/basicsLists/Application.java
@@ -10,12 +10,12 @@ public class Application {
         List<Integer> list = new ArrayList<>();
         list.addAll(List.of(1,2,3,-4,4,3,-1,2,7,8,99,-13,0,0,1,0,3,3,-8,-1,1,6));
         System.out.println(max(list));
-
+        System.out.println(min(list));
     }
 
 
     public static int max(List <Integer> list) {
-        int max = 0;
+        int max = Integer.MIN_VALUE;
 
         for(Integer number : list) {
             if(number > max) {
@@ -24,4 +24,16 @@ public class Application {
         }
         return max;
     }
+
+    public static int min (List<Integer> list) {
+        int min = Integer.MAX_VALUE;
+
+        for(Integer number : list) {
+            if(number < min) {
+                min = number;
+            }
+        }
+        return min;
+    }
+
 }

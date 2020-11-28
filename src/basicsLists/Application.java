2902979776919bc786789913e73fea48cf04975a
delola\delola lokal <aleksandra.man@gmail.com>
2020-11-28 22:23:07 +0100

    listContainsNumber method basicList package

--- src/basicsLists/Application.java
+++ src/basicsLists/Application.java
@@ -8,22 +8,24 @@ public class Application {
     public static void main(String[] args) {
 
         List<Integer> list = new ArrayList<>();
-        list.addAll(List.of(1,2,3,-4,4,3,-1,2,7,8,99,-13,0,0,1,0,3,3,-8,-1,1,6));
+        list.addAll(List.of(1, 2, 3, -4, 4, 3, -1, 2, 7, 8, 99, -13, 0, 0, 1, 0, 3, 3, -8, -1, 1, 6));
         System.out.println(max(list));
         System.out.println(min(list));
         System.out.println(sum(list));
         System.out.println(average(list));
         System.out.println(multiplyEven(list));
+        System.out.println(listContainsNumber(list, 8));
+        System.out.println(listContainsNumber(list, 9));
 
 
     }
 
 
-    public static int max(List <Integer> list) {
+    public static int max(List<Integer> list) {
         int max = Integer.MIN_VALUE;
 
-        for(Integer number : list) {
-            if(number > max) {
+        for (Integer number : list) {
+            if (number > max) {
                 max = number;
             }
         }
@@ -33,8 +35,8 @@ public class Application {
     public static int min(List<Integer> list) {
         int min = Integer.MAX_VALUE;
 
-        for(Integer number : list) {
-            if(number < min) {
+        for (Integer number : list) {
+            if (number < min) {
                 min = number;
             }
         }
@@ -42,20 +44,20 @@ public class Application {
     }
 
 
-    public static int sum(List <Integer> list) {
+    public static int sum(List<Integer> list) {
         int sum = 0;
 
-        for(Integer number : list) {
+        for (Integer number : list) {
             sum = sum + number;
         }
         return sum;
     }
 
-    public static int average (List <Integer> list) {
+    public static int average(List<Integer> list) {
         int average;
         int sum = 0;
 
-        for(Integer number : list) {
+        for (Integer number : list) {
 
             sum = sum + number;
         }
@@ -64,14 +66,28 @@ public class Application {
         return average;
     }
 
-    public static int multiplyEven(List <Integer> list) {
+    public static int multiplyEven(List<Integer> list) {
         int evenMultiply = 1;
 
-        for(Integer number : list) {
-            if(number % 2 == 0){
+        for (Integer number : list) {
+            if (number % 2 == 0) {
                 evenMultiply *= number;
             }
         }
         return evenMultiply;
     }
+
+    public static boolean listContainsNumber(List<Integer> list, int number) {
+        boolean result = false;
+
+        for (Integer listNumber : list) {
+            if (listNumber == number) {
+                result = true;
+                break;
+            }
+        }
+
+        return result;
+    }
+
 }

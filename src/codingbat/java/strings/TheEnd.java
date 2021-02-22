277f87e77f7f261b8327629ae7a65497215e5f48
delola\delola lokal <aleksandra.man@gmail.com>
2021-02-22 18:35:51 +0100

    TheEnd String1 codingBat solution

---
+++ src/codingbat/java/strings/TheEnd.java
@@ -0,0 +1,26 @@
+package codingbat.java.strings;
+
+/*
+Given a string, return a string length 1 from its front, unless front is false, in which case return a string length 1 from its back. The string will be non-empty.
+
+theEnd("Hello", true) → "H"
+theEnd("Hello", false) → "o"
+theEnd("oh", true) → "o"
+ */
+
+public class TheEnd {
+
+    public static void main(String[] args) {
+        theEnd("Hello", true);
+        theEnd("Hello", false);
+        theEnd("oh", true);
+    }
+
+    public static String theEnd (String str, boolean front) {
+        if(front) {
+            return str.substring(0, 1);
+        } else {
+            return str.substring(str.length() - 1);
+        }
+    }
+}

88b12d1d17c4ab5f93791c62e2c5f6640a484f0d
delola\delola lokal <aleksandra.man@gmail.com>
2021-02-26 20:00:25 +0100

    Topping1 updated Map1 CodingBat solution

--- src/codingbat/java/maps/Topping1.java
+++ src/codingbat/java/maps/Topping1.java
@@ -3,18 +3,22 @@ package codingbat.java.maps;
 import java.util.HashMap;
 import java.util.Map;
 
+/*
+Given a map of food keys and topping values, modify and return the map as follows: if the key "ice cream" is present, set its value to "cherry". In all cases, set the key "bread" to have the value "butter".
+
+topping1({"ice cream": "peanuts"}) → {"bread": "butter", "ice cream": "cherry"}
+topping1({}) → {"bread": "butter"}
+topping1({"pancake": "syrup"}) → {"bread": "butter", "pancake": "syrup"}
+ */
+
 public class Topping1 {
-    public static void main(String[] args) {
+    public Map<String, String> topping1(Map<String, String> map) {
 
-        Map<String, String> topping1 = new HashMap<>();
-        topping1.put("ice cream", "peanuts");
-        topping1.put("","");
-        topping1.put("pancake", "syrup");
-
-        topping1.put("bread", "butter");
-
-        if(topping1.containsKey("ice cream")) {
-            topping1.put("ice cream", "cherry");
+        if(map.containsKey("ice cream")){
+            map.put("ice cream", "cherry");
         }
+        map.put("bread", "butter");
+
+        return map;
     }
 }

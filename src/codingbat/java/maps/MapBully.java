29ed5ac5440e8a13200be3c343060cca62e33235
delola\delola lokal <aleksandra.man@gmail.com>
2021-02-26 19:50:13 +0100

    MapBully Map1 CodingBat solution

---
+++ src/codingbat/java/maps/MapBully.java
@@ -0,0 +1,33 @@
+package codingbat.java.maps;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/*
+Modify and return the given map as follows: if the key "a" has a value, set the key "b" to have that value, and set the key "a" to have the value "". Basically "b" is a bully, taking the value and replacing it with the empty string.
+
+mapBully({"a": "candy", "b": "dirt"}) → {"a": "", "b": "candy"}
+mapBully({"a": "candy"}) → {"a": "", "b": "candy"}
+mapBully({"a": "candy", "b": "carrot", "c": "meh"}) → {"a": "", "b": "candy", "c": "meh"}
+ */
+
+public class MapBully {
+
+    public static void main(String[] args) {
+        Map<String, String> map = new HashMap<>();
+        map.put("a","candy");
+        map.put("b", "dirt");
+        mapBully(map);
+
+    }
+
+    public static Map<String, String> mapBully(Map<String, String> map) {
+
+        if(map.containsKey("a")){
+            map.put("b", map.get("a"));
+            map.put("a", "");
+        }
+
+        return map;
+    }
+}

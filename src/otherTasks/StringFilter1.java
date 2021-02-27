ab6cc8c184e892dffb08e71989d3569a83030be9
delola\delola lokal <aleksandra.man@gmail.com>
2021-02-27 22:04:33 +0100

    StringFilter1 repeating1 method added

--- src/otherTasks/StringFilter1.java
+++ src/otherTasks/StringFilter1.java
@@ -1,6 +1,7 @@
 package otherTasks;
 
 import java.lang.reflect.Array;
+import java.util.*;
 import java.util.Arrays;
 
 public class StringFilter1 {
@@ -8,7 +9,13 @@ public class StringFilter1 {
     public static void main(String[] args) {
         String[] stringArray = new String[] {"agattata", "loopapala", "ewa", "ana", "ola", "ewa", "ola", "pnamanaama", "mama", "nana", "roma", "asia", "sam", "tomekkkkjo"};
         System.out.println(Arrays.toString(noLongerThan1(stringArray, 5)));
-        System.out.println(Arrays.toString(noLongerThan1(null, 5)));
+        //System.out.println(Arrays.toString(noLongerThan1(null, 5)));
+
+
+        List<String> stringList = new ArrayList<String>();
+        Collections.addAll(stringList, stringArray);
+
+        System.out.println(repeating1(stringList));
     }
 
     public static String[] noLongerThan1 (String[] array, int n){
@@ -28,6 +35,28 @@ public class StringFilter1 {
         }
 
         return result;
+    }
+
+    public static List<String> repeating1(List<String> list) {
+
+        List<String> resultList = new ArrayList<>();
+        Map<String, Integer> map = new HashMap<>();
+
+        for(String str : list){
+            if(map.containsKey(str)){
+                map.put(str, map.get(str) + 1);
+            } else {
+                map.put(str, 1);
+            }
+        }
+
+        for(String repStr : map.keySet()){
+            if(map.get(repStr) > 1){
+                resultList.add(repStr);
+            }
+        }
+
+        return resultList;
     }
 
 
